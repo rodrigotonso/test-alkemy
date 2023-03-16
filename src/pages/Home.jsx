2f2f0e87d85fac7a769acda610c1fed46e8fad0c
Rodrigo Tonso <67134489+rodrigotonso@users.noreply.github.com>
2023-03-16 17:58:35 -0300

    .env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useLocation } from "react-router-dom";
 
 const Home = () => {
     const location = useLocation();
+    console.log(process.env.REACT_APP_KEYFACEBOOK)
+    console.log(process.env.REACT_APP_KEY)
     console.log(location);
     return <div>Home</div>;
 };
